a1034fd26c3d39a4d3519a51ede721cea38305ae
wangheng <1936807881@qq.com>
2021-12-20 21:37:43 +0800

    今天的学习

---
+++ 20211220study.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct Smartphone {
+    string name;
+    int storageSpace;
+    string color;
+    float price;
+};
+
+struct person {
+    string name;
+    int age;
+    Smartphone smartphone;
+};
+
+void printSmartphoneInfo(Smartphone smartphone){
+    cout<<"name: "<<smartphone.name<<endl;
+    cout<<"storageSpace: "<<smartphone.storageSpace<<endl;
+    cout<<"color: "<<smartphone.color<<endl;
+    cout<<"price: "<<smartphone.price<<endl;
+}
+
+void printPersonIofo(person person){
+    cout<<"name: "<<person.name<<endl;
+    cout<<"age: "<<person.age<<endl;
+    printSmartphoneInfo(person.smartphone);
+}
+
+int main()
+{
+    
+    Smartphone smartphone;
+    smartphone.name="iphone 12";
+    smartphone.storageSpace = 32;
+    smartphone.color = "black";
+    smartphone.price = 999.99;
+
+    Smartphone smartphone2;
+    smartphone2.name="Smasung Galaxy s21 Ultra";
+    smartphone2.storageSpace = 64;
+    smartphone2.color = "gray";
+    smartphone2.price = 888.88;
+
+    person p;
+    p.name = "wangheng";
+    p.age = 22;
+    p.smartphone =smartphone2;
+    printPersonIofo(p);
+
+    system("pause>0");
+}
